a937c1c77975ee32ac1cb875d3ecbc4133a56ac1
saicharansampathirao <saicharansampathirao@gmail.com>
2025-05-11 16:14:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GynaanX-single
---
+++ mail/mail.php
@@ -0,0 +1,52 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/PHPMailer/src/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate input fields
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo json_encode(["error" => "Invalid email format"]);
+        exit;
+    }
+
+    $subject = 'Message from ' . $email;
+    $email_content = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$message\n";
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        // $mail->Username = 'shaiksaleem2831@gmail.com';
+        // $mail->Password = 'accp xqud jphc mzsn';  // Ensure this is secure and not publicly accessible
+        $mail->Username = '';
+        $mail->Password = '';  // 
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->addAddress('richessesolutions.official@gmail.com');
+        $mail->addAddress('investor@richesse.solutions');
+
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body = $email_content;
+
+        $mail->send();
+        echo json_encode(["success" => "Mission Accomplished!"]);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(["error" => "Oops! Something went wrong: " . $e->getMessage()]);
+    }
+    exit; // Ensure that the script stops here
+}
